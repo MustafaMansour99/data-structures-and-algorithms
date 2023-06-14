91f8fba2b9d680437654d8010e9fd6be723217eb
abostaf <23037172@student.ltuc.com>
2023-06-14 10:48:14 +0000

    added doc-string

--- insertion_sort/insertion_sort.py
+++ insertion_sort/insertion_sort.py
@@ -1,4 +1,19 @@
 def insertion_sort(arr):
+    """
+    Sorts an input list in ascending order using the Insertion Sort algorithm.
+
+    Args:
+        input (list): The input list to be sorted.
+
+    Returns:
+        list: The sorted list in ascending order.
+
+    Examples:
+        >>> arr = [20, 18, 12, 8, 5, -2]
+        >>> sorted_arr = insertion_sort(arr)
+        >>> print(sorted_arr)
+        [-2, 5, 8, 12, 18, 20]
+    """
     for i in range(1, len(arr)):
         key = arr[i]
         j = i - 1
@@ -8,6 +23,14 @@ def insertion_sort(arr):
         arr[j + 1] = key
     return arr
 def insert(sorted, value):
+    """
+    Inserts a value into the sorted list at the appropriate position.
+
+     Args:
+        sorted (list): The sorted list to insert the value into.
+        value (int): The value to be inserted.
+    """
+
     i = 0
 
     while i < len(sorted) and value > sorted[i]:
